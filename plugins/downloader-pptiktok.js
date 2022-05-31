9ecd9d17efe8a9c100d0b7bc4b1c44b8e32cc8a7
https://ıl|ẉa.me/˖Papah-chan/Yotsuba「xʏᴢ×ᴏғᴄ」|lı <92633211+FahriAdison@users.noreply.github.com>
2022-06-01 06:11:52 +0700

    Change to global lolkey
    Biar simpel

--- plugins/downloader-pptiktok.js
+++ plugins/downloader-pptiktok.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, text }) => {
 if (!text) throw '*[❗INFO❗] Masukan Nama User Tiktok Yang Ingin Diambil Fotonya*'
-let res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=YourApikey`
+let res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=${lolkey}`
 conn.sendFile(m.chat, res, 'error.jpg', `*[ ✔ ] Sukses Mengambil Foto User ${text}*`, m, false)
 }
 handler.help = ['tiktokfoto'].map(v => v + ' <username>')
