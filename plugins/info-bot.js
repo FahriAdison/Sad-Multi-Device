d799c6f742912b36563c8ec23daa4dfe232b2ef0
https://ıl|ẉa.me/˖Papah-chan/Yotsuba「xʏᴢ×ᴏғᴄ」|lı <92633211+FahriAdison@users.noreply.github.com>
2022-06-01 14:48:07 +0000

    Change Info Bot

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,22 +1,92 @@
-import fs from "fs"
-let handler = m => m
+import { cpus as _cpus, totalmem, freemem } from 'os'
+import util from 'util'
+import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
+import { join } from 'path'
+import { promises } from 'fs'
+import moment from 'moment-timezone'
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+let format = sizeFormatter({
+  std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
+  decimalPlaces: 2,
+  keepTrailingZeroes: false,
+  render: (literal, symbol) => `${literal} ${symbol}B`,
+})
+let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
+    let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
+    let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
+    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+  const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+  const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
+  const used = process.memoryUsage()
+  const cpus = _cpus().map(cpu => {
+    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+    return cpu
+  })
+  const cpu = cpus.reduce((last, cpu, _, { length }) => {
+    last.total += cpu.total
+    last.speed += cpu.speed / length
+    last.times.user += cpu.times.user
+    last.times.nice += cpu.times.nice
+    last.times.sys += cpu.times.sys
+    last.times.idle += cpu.times.idle
+    last.times.irq += cpu.times.irq
+    return last
+  }, {
+    speed: 0,
+    total: 0,
+    times: {
+      user: 0,
+      nice: 0,
+      sys: 0,
+      idle: 0,
+      irq: 0
+    }
+  })
+  let old = performance.now()
+  let neww = performance.now()
+  let speed = neww - old
+    conn.sendHydrated(m.chat, 
+    '*––––––『 BOT INFO 』––––––*', 
+`🤖 ɴᴀᴍᴇ: ${_package.name}
+🧩 ᴠᴇʀsɪᴏɴ: ${_package.version}
+📚 ʟɪʙʀᴀʀʏ: ${_package.description}
 
-handler.all = async function (m) {
-let vn = './media/bot.mp3'
-const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
-message: { 
-orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Sad-Bot', orderTitle: 'Bang', thumbnail: fs.readFileSync('./thumbnail.jpg'), sellerJid: '0@s.whatsapp.net'    
-}}}
-const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
-message: { 
-"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"   
-}}}  
+⏳ ᴜᴩᴛɪᴍᴇ: ${uptime}
+📈 ᴅᴀᴛᴀʙᴀsᴇ: ${totalreg}
 
-if (/^bot$/i.test(m.text)) {
-    
-conn.sendButton(m.chat, '*Hai,Ada Yang Bisa Saya Bantu?*', wm, [['Back To Menu', `#menu`]], 'conversation', { sendEphemeral: true, quoted: estilo })
-conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true, quoted: estiloaudio })   
+📅 ᴅᴀᴛᴇ: ${date}
+⌚ ᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00﹚
+
+💻 sᴇʀᴠᴇʀ ɪɴғᴏ :
+⮕ ᴩɪɴɢ: ${speed} ᴍs
+⮕ ʀᴀᴍ: ${format(totalmem() - freemem())} / ${format(totalmem())}
+
+💬 ᴡʜᴀᴛsᴀᴩᴩ sᴛᴀᴛᴜs :
+⮕ ${groupsIn.length} - Group Chats
+⮕ ${groupsIn.length} - Groups Joined
+⮕ ${groupsIn.length - groupsIn.length} - Groups Left
+⮕ ${chats.length - groupsIn.length} - Personal Chats
+⮕ ${chats.length} - Total Chats
+`.trim(), './media/botinfo.jpg', 'https://github.com/FahriAdison/Sad-Multi-Device', 'ʙᴏᴛ sᴄʀɪᴩᴛ', null, null, [
+[`ᴏᴡɴᴇʀ`, `${usedPrefix}owner`],
+[`ᴅᴏɴᴀᴛᴇ`, `${usedPrefix}donate`]
+], m, {asLocation: true})
 }
-return !0
-}
+handler.help = ['botinfo']
+handler.tags = ['info']
+handler.command = /^(botinfo|bot|info|bi)$/i
+
 export default handler
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
