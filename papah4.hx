d0e2c1b6ad14b87580ab21c2fbb15237fd3e75b1
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-06-08 15:32:45 +0000

    y

---
+++ papah4.hx
@@ -0,0 +1,1005 @@
+class Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahaha
+    
+    class Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahahaclass Main{
+    static function main(){
+    trace("Papah-Chan");
+    }
+    }
+    
+    //Yahaha
