3c224e33cbddafd8c0083821bd856a09611b4934
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-06-08 11:51:12 +0000

    y

---
+++ papah-frame.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //frame 
+
+        Jframe frame = new Jframe(); //framenya
+        frame.setTittle("Papah-Chan");
+        frame.setDefaultCloseOperation(Jframe.EXIT_ON_CLOSE); //tombol close
+        frame.setResizeble(false); //:v
+        frame.setSize(200,200);
+    }
+}
