c518c9d9899361a0d5945e5b0d7950320a9a45f1
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-06-13 21:53:32 +0000

    Fix Anti 212

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -4,13 +4,13 @@ handler.before = async function (m) {
    if (m.sender.startsWith('212' || '212')) {
    	global.db.data.users[m.sender].banned = true
 conn.reply('see u next time')
-conn.groupParticipantsUpdate(m.chat, [user], "remove")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
    }
    
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
 conn.reply('see u next time')
-conn.groupParticipantsUpdate(m.chat, [user], "remove")
+conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
    } 
     }
 
