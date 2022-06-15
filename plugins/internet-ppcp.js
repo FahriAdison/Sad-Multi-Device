7d46b508ecfd5c9afc6781e6284319f594d2053b
NevtGanz <95397046+NevtBotz@users.noreply.github.com>
2022-06-15 10:10:48 +0800

    update ppcp pake db

--- plugins/internet-ppcp.js
+++ plugins/internet-ppcp.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 let res = await fetch(`https://api.lolhuman.xyz/api/random/ppcouple?apikey=${lolkey}`)
 if (res.status != 200) throw await res.text()
@@ -10,4 +10,23 @@ conn.sendButton(m.chat, 'Cowok', wm, json.result.male, [['NEXT', `/${command}`]]
 handler.help = ['ppcouple']
 handler.tags = ['internet']
 handler.command = /^(ppcp|ppcouple)$/i
-export default handler
+export default handler*/
+
+
+import fetch from "node-fetch"
+let handler = async (m, { conn }) => {
+
+  let data = await (await fetch('https://raw.githubusercontent.com/KazukoGans/database/main/anime/ppcouple.json')).json()
+  let cita = data[Math.floor(Math.random() * data.length)]
+  
+  let cowi = await(await fetch(cita.cowo)).buffer()
+  await conn.sendFile(m.chat, cowi, '', 'cowo', m)
+  let ciwi = await(await fetch(cita.cewe)).buffer()
+  await conn.sendFile(m.chat, ciwi, '', 'cewe', m)
+}
+handler.help = ['ppcouple', 'ppcp']
+handler.tags = ['internet']
+handler.command = /^(pp(cp|couple))$/i
+handler.limit = true
+
+export default handler 
