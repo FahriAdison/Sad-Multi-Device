761a0530ed9a53921c42c47e58575f53ae5d2a2a
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-06-15 14:59:18 +0000

    Di Perbagus Aja :v

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,13 +1,13 @@
-import fs from 'fs'
-let handler = async (m, { conn }) => {
-conn.reply(m.chat, `*https://github.com/FahriAdison/Sad-Multi-Device*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '',
-body: '',         
-previewType: 0, thumbnail: fs.readFileSync("./thumbnail.jpg"),
-sourceUrl: `https://github.com/FahriAdison`}}})
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*https://github.com/FahriAdison/Sad-Multi-Device*\nSubscribe Untuk Mengetahui Update Selanjutnya:bit.ly/Papah-Chan\nSilahkan follow github saya dibawah ,Terimakasih`
+conn.sendHydrated(m.chat, str, wm, pp, 'https://github.com/FahriAdison/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+['𝙼𝙴𝙽𝚄', '/menu']
+], m)
 }
-handler.command = ['sc','script'] 
-handler.help = ['sc']
-handler.tags = ['general']
+handler.command = handler.help = ['sc']
+handler.tags = ['info']
+
 export default handler
