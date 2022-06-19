3fac02786bb5762ffee877a838eff4cce01441b1
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-06-19 01:53:25 +0000

    Ganti Ke Database

--- plugins/random-sound.js
+++ plugins/random-sound.js
@@ -1,18 +1,81 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `Contoh:
-${usedPrefix + command} 2
-Max Angka 70
-*Hanya Angka*`
-
-let vn = `https://hansxd.nasihosting.com/sound/sound${text}.mp3`
-await conn.sendFile(m.chat, vn, 'song.mp3', null, m, true, {
-type: 'audioMessage', 
-ptt: true 
-})
-
+let handler = async (m, { conn, command }) => {
+m.reply('Wait 🐦')
+let audio = `https://raw.githubusercontent.com/saipulanuar/Api-Github/main/audio/${command}.mp3`
+await conn.sendFile(m.chat, audio, 'error.mp3', null, m, true)
 }
-handler.help = ['sound <angka>']
-handler.tags = ['random']
-handler.command = /^(sound)$/i
 
+handler.command = handler.help = [
+'anjay',
+'ara-ara',
+'ara-ara-cowok',
+'ara-ara2',
+'arigatou',
+'assalamualaikum',
+'asu',
+'ayank',
+'aku-ngakak',
+'bacot',
+'bahagia-aku',
+'baka',
+'bansos',
+'beat-box',
+'beat-box2',
+'biasalah',
+'bidadari',
+'bot',
+'buka-pintu',
+'canda-anjing',
+'cepetan',
+'cuekin-terus',
+'daisuki-dayo',
+'daisuki',
+'dengan-mu',
+'gaboleh-gitu',
+'gak-lucu',
+'gamau',
+'gay',
+'gelay',
+'gitar',
+'gomenasai',
+'hai-bot',
+'hampa',
+'hayo',
+'hp-iphone',
+'i-like-you',
+'ih-wibu',
+'india',
+'karna-lo-wibu',
+'kiss',
+'kontol',
+'ku-coba',
+'maju-wibu',
+'makasih',
+'mastah',
+'nande-nande',
+'nani',
+'ngadi-ngadi',
+'nikah', 'nuina',
+'onichan',
+'owner-sange',
+'ownerku',
+'pak-sapardi',
+'pale',
+'pantek',
+'pasi-pasi',
+'punten',
+'sayang',
+'siapa-sih',
+'sudah-biasa',
+'summertime',
+'tanya-bapak-lu',
+'to-the-bone',
+'wajib',
+'waku',
+'woi',
+'yamete',
+'yowaimo',
+'yoyowaimo'
+]
+handler.tags = ['random']
+handler.owner = false
 export default handler
