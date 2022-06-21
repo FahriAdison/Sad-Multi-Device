211e1621af0b7ef21ad069d35796c7c2939b5908
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-06-21 20:32:29 +0700

    Hadir mint
    pov

--- plugins/_res.js
+++ plugins/_res.js
@@ -1,21 +1,38 @@
+import fetch from "node-fetch"
+import { readFileSync } from "fs"
+
+
+
 export async function all(m) {
 
-  const stc = ['https://a.uguu.se/AinzEawY.webp',
-              'https://a.uguu.se/aNqaxmfZ.webp',
-              'https://a.uguu.se/yJELqBgJ.webp',
-              'https://a.uguu.se/vfVKiAGe.webp',
-              'https://a.uguu.se/uybRbuue.webp',
-              'https://a.uguu.se/CDGifugk.webp']
-  var stcs = stc[Math.floor(Math.random() * (stc.length))]
+  //Kalo mau menggokil pake ini
+  let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+
+  let stc = readFileSync('./ress.webp')
+
 if (m.isBaileys) return
     if (m.chat.endsWith('broadcast')) return
 
-    // when tagged send sticker 
+    // ketika ditag 
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
-            await this.sendFile(m.chat, stcs, 'tag.webp', '', m, false, {sendEphemeral: true})
+            await this.sendMessage(m.chat, { sticker : stc, thumbnail: thumb , contextInfo:{  externalAdReply: { showAdAttribution: true,
+mediaType:  1,
+mediaUrl: 'https://wa.me/6283820073017',
+title: '「 ❔ 」',
+body: me,
+sourceUrl: 'http://github.com/Rlxfly', thumbnail: await( await fetch(pp)).buffer()
+ //Kalo mau ada pp dari sender/pengirimnya
+ //Kalo mau ringan pake global.thumb                                                                                                              
+                                                                                                               
+          //thumbnail: await( await fetch(pp)).buffer()
+  }
+ }}, { quoted: m })
+          
         }
     } catch (e) {
         return
     }
+
+                                
 }
