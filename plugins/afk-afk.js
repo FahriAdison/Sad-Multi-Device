a91ccd7c5b47380a2d789ad3fe7eee4839cbff17
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-06-23 22:25:27 +0000

    doc thumb

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,11 +1,24 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
 let handler = async (m, { text }) => {
-    let user = global.db.data.users[m.sender]
-    user.afk = + new Date
-    user.afkReason = text
-    m.reply(`
-  ${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
-  `)
-}
+let name = m.pushName || conn.getName(m.sender)
+
+let user = global.db.data.users[m.sender]
+let wibu = `https://api.zacros.my.id/randomimg/loli`    
+let thumb = await(await fetch(wibu)).buffer()
+user.afk = + new Date
+user.afkReason = text
+ conn.sendButtonDoc(m.chat, `${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}`, wm, 'Jangan Ganggu Ya', 'Bilek', m,  { contextInfo: { externalAdReply: { showAdAttribution: true,
+        mediaUrl: "https://Instagram.com/bot_whangsaf",
+        mediaType: "VIDEO",
+        description: "https://Instagram.com/bot_whangsaf", 
+        title: 'Simple Bot Esm',
+        body: wm,
+        thumbnail: thumb,
+        sourceUrl: sgc
+    }
+    } })
+            }
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
 handler.command = /^afk$/i
