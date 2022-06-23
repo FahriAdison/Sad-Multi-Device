90033a60b9ceda6f016a3edd2fcbdb897b521a24
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-06-23 22:16:17 +0000

    getaud vn

---
+++ plugins/_res-audio.js
@@ -0,0 +1,27 @@
+import { 
+    youtubedl,
+    youtubedlv2 
+} from '@bochilteam/scraper'
+
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
+
+
+  let q = '128kbps'
+  let v = args[0]
+
+const yt = await youtubedl(v).catch(async () => await  youtubedlv2(v))
+const dl_url = await yt.audio[q].download()
+  const ttl = await yt.title
+const size = await yt.audio[q].fileSizeH
+  
+ await m.reply(`▢ Tɪᴛᴛʟᴇ: ${ttl}
+▢  Sɪᴢᴇ: ${size}
+▢ Ｌｏａｄｉｎｇ. . .`)
+  await conn.sendFile(m.chat, dl_url, ttl + '.mp3', wm, m, null, {
+    asDocument: false
+  })
+}
+
+
+handler.command = /^(getaud)$/i
+export default handler
