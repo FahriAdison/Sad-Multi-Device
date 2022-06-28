895f1be5d1fe2b7701e1b3c5fde3e4c48fcd607f
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-06-28 09:59:01 +0000

    aphyh

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -4,9 +4,9 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 *PAYMENT ↓*
-_*Pulsa/pulse(Telkomsel):*_ 082287219167
-_*Dana/ovo:*_ 0895612153565
-Setelah melakukan donasi kirim bukti pembayaran ke owner
+_*Pulsa/Pulse(Telkomsel):*_ 082287219167
+_*Dana/Gopay:*_ 0895612153565
+Setelah melakukan donasi kirim bukti pembayaran ke owner,Dengan melakukan donasi,kamu telah mensupport bot ini agar on 24 jam dan fast respon
 `
 conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/FahriAdison/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
