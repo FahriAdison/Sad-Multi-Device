771dfb3150f984f81e486786e6f3785eddd7a1fd
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-07-04 14:53:03 +0000

    runtime troli

--- plugins/misc-runtime.js
+++ plugins/misc-runtime.js
@@ -1,26 +1,64 @@
+let { generateWAMessageFromContent } = (await import("@adiwajshing/baileys")).default 
+import { performance } from 'perf_hooks'
 import fs from 'fs'
-let handler = async (m, { conn, args, command }) => {
-	let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
-    }
-    let muptime = clockString(_muptime)
- m.reply(`*R U N T I M E*\n${muptime}\n`)
+let handler  = async (m, { conn, usedPrefix: _p }) => {
 
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+
+  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
+  return `\n\t\t*「 \t ${pad(hours)}H ${pad(minutes)}M ${pad(seconds)}S \t」*\n`
 }
+					const runtime = process.uptime()
+		            const teks = `${kyun(runtime)}`
+					const itsme = `0@s.whatsapp.net`
+					const split = `uwu >//<`
+					const rtimebro = {
+					contextInfo: {
+					participant: itsme,
+					quotedMessage: {
+					extendedTextMessage: {
+				    text: split
+									}
+								}
+							}
+					}
+						    
+						  let prep = generateWAMessageFromContent(m.chat, { orderMessage: { 
+itemCount: -10062007, status: 500,
+surface: 999,
+message: teks,
+description: '^^',
+orderTitle: 'Hehe',
+token: '9',
+curreyCode: 'IDR',
+totalCurrencyCode: '>〰<',
+totalAmount1000: '1000000',
+sellerJid: '17608914335@s.whatsapp.net',
+thumbnail: fs.readFileSync('./thumbnail.jpg')
+}}, {contextInfo: null, quoted: m})
+conn.relayWAMessage(prep)
+				/*	conn.sendMessage(m.chat, `${teks}`, MessageType.text, rtimebro)*/
+}
+
 handler.help = ['runtime']
 handler.tags = ['info']
-handler.command = ['runtime', 'rt']
-export default handler
+handler.command = /^runtime$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
-function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
-}
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+export default handler
