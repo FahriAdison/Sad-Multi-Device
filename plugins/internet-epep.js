5446e0bf1ad985bbcf70087c9b5aa84298dd8c01
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-07-05 03:58:51 +0000

    Ngep Ngep

---
+++ plugins/internet-epep.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch'
+let handler = async (m, { args }) => {
+  let res = await fetch(global.API('xteam', '/search/freefire', {id: args[0]}, `${xkey}`))
+  let json = await res.json()
+  if (json.result.name) conn.reply(m.chat, json.result.name, m)
+    else throw json
+}
+handler.help = ['epep'].map(v => v + ' <id>')
+handler.tags = ['internet']
+handler.command = /^(freefire|epep)$/i
+
+export default handler
