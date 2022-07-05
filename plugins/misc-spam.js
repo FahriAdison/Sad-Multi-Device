0dd1c7ff297fe71749f3a4fe45bb5e1c0740baf0
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-07-05 03:17:46 +0000

    Kalau Lag Pakai Ini Aja

---
+++ plugins/misc-spam.js
@@ -0,0 +1,24 @@
+let handler = async (m, { conn, text }) => {
+  let [t1, t2] = text.split `|`
+    let name = conn.getName(m.sender)
+ conn.fakeReply(m.chat,'\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n' , '0@s.whatsapp.net', t1+ 'Hai' ,'17608914335-1615035634@g.us')
+}
+ 
+handler.help = ['spam']
+
+handler.tags = ['main']
+
+handler.command = /^spam$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 3
+
+export default handler
