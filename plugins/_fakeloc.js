ed76893ee93bbf1d1fb878788439d1eee0a93fc7
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-07-07 17:28:50 +0700

    😅

---
+++ plugins/_fakeloc.js
@@ -0,0 +1,33 @@
+import { generateWAMessageFromContent } from "@adiwajshing/baileys"
+
+
+
+let handler  = async (m, { conn }) => {
+
+
+
+let prep = generateWAMessageFromContent(m.chat, { liveLocationMessage: { 
+
+degreesLatitude: 35.685506276233525, degreesLongitude: 139.75270667105852,
+
+caption: wm,
+
+sequenceNumber: 1656662972682001, timeOffset: 8600, jpegThumbnail: null
+
+}}, { quoted: m
+
+					})
+
+
+
+return conn.relayMessage(m.chat, prep.message, { messageId: prep.key.id })
+
+}
+
+
+
+handler.command = /^loc2$/
+
+handler.owner = true
+
+export default handler
