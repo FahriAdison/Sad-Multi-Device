725c5f5ae1bad5b6df84793a947f5bf19728f758
Alfahosting102 <106969484+Alfahosting102@users.noreply.github.com>
2022-07-07 12:11:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ import { watchFile, unwatchFile } from 'fs'
 import moment from 'moment-timezone'
 
 global.owner = [
-  ['+62 831-4339-37633'],
-  ['+1 760-891-4335'],
-  ['+84 82 430 4616'],
-  ['+84 34 979 0091'],
-  ['+62 818-0545-0249'],
-  ['+62 831-4339-37633', 'Letta', true],
-  ['17608914335', 'Papah-Chan', true],
-  ['+84 82 430 4616', 'Bot', true],
-  ['+84 34 979 0091', 'Johannes', true],
+  ['+62 896-4326-8410'],
+  ['+62 857-2452-3762'],
+  ['62 896-4326-8410'],
+  ['+62 857-2452-376'],
+  ['62 896-4326-8410'],
+  ['62 896-4326-8410', 'ALFA STORE', true],
+  ['+62 857-2452-376', 'ALFA BOTZ', true],
+  ['+62 857-2452-376', 'Bot', true],
+  ['62 896-4326-8410', 'ALFA RIZI', true],
   ['+62 818-0545-0249', 'Krisna', true],
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
@@ -92,10 +92,10 @@ global.hbunny = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://t
 global.hbeach = ['https://telegra.ph/file/14ae0ba2da77d74e6b80c.jpg', 'https://telegra.ph/file/b6905b77e6c7732592a13.jpg','https://telegra.ph/file/9da45a352eb4c40e5d641.jpg', 'https://telegra.ph/file/59e78846ee365975ee6e3.jpg','https://telegra.ph/file/1bf7dee46b83eb4c41d7d.jpg','https://telegra.ph/file/0525a7929f819cb8278f3.jpg']
 global.bg = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://telegra.ph/file/ae610571b62b5ab876e9c.jpg', 'https://telegra.ph/file/cc8255d5b989eef587af2.jpg','https://telegra.ph/file/30d2e7375996abd9cfee3.jpg']
 /*============== NOMOR ==============*/
-global.nomorbot = '6281268416245'
-global.nomorown = '17608914335'
-global.namebot = 'Sad-Bot'
-global.nameown = 'Papah-Chan'
+global.nomorbot = '+62 857-2452-3762'
+global.nomorown = '62 896-4326-8410'
+global.namebot = 'ALFA BITZ'
+global.nameown = 'ALFA STORE'
 
 /*=========== FAKE SIZE ===========*/
 global.fsizedoc = '99999999999999' // default 10TB
