07647845113439b877932a916f7b97a7f1e7729c
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-07-26 17:24:15 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -4,8 +4,8 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 *PAYMENT ↓*
-_*Pulsa/Pulse(Telkomsel):*_ 082287219167
-_*Dana/Gopay:*_ 0895612153565
+_*Pulsa/Pulse(Indosat):*_ 085624823115
+_*Dana/Gopay:*_ 089531704997
 Setelah melakukan donasi kirim bukti pembayaran ke owner,Dengan melakukan donasi,kamu telah mensupport bot ini agar on 24 jam dan fast respon
 `
 conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/FahriAdison/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
