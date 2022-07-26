45efd9467b05dc220cdebf53930fc8031aaa4350
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-07-26 17:22:10 +0700

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -1,24 +1,24 @@
 let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
-*💌 Nama* : Sad-Bot
-*✉️ Nama RL* : Fahri
+*💌 Nama* : Irfaan Official
+*✉️ Nama RL* : Irfaan Official
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
 *⏰ Tanggal lahir* : 10 Juni 2007
 *🎨 Umur* : 15
 *🧮 Kelas* : 9
 *🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
-*💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Riau, Indragiri Hulu, Air Molek
+*💬 Sifat* : Baik, Ramah, Kanjut, Prik, Pedofil
+*🗺️ Tinggal* : Indonesia, Tasikmalaya, Singaparna, Mangkubumi
 *❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
 *💔 Benci* : autis, anak epep, seleb
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @bot_whangsaf
-*🇫  Facebook* : Adison Fahri
-*🏮 Chanel Youtube* : Papah-Chan
-*🐈 Github:* FahriAdison
+*📷 instagran* : @Irfaanabdulhafiizh
+*🇫  Facebook* : Irfaan Abdul Hafiizh
+*🏮 Chanel Youtube* : HimHia Channel
+*🐈 Github:* Alfarabotz567
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
