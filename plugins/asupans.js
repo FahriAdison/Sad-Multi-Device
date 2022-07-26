1ea22f1dc2d7e013baf9b9defeda6fb74d4aad5f
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-07-26 17:36:09 +0700

    Create asupans.js

---
+++ plugins/asupans.js
@@ -0,0 +1,11 @@
+let handler = async (m, { conn }) => {
+	conn.sendFile(m.chat, 'https://api.zacros.my.id/asupan/random', 'asupan.mp4', 'awas Sange Bang😅', m)
+}
+handler.help = ['asupan']
+handler.tags = ['asupan']
+
+handler.command = /^(asupan)$/i
+handler.premium = false
+handler.register = true
+handler.limit = 5
+module.exports = handler
