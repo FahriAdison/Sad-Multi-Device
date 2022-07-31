f0813d229fb0fd60ab2f2d8e28bdf908d1f51b7f
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-07-31 16:38:52 +0700

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -4,7 +4,7 @@ let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
 conn.sendHydrated(m.chat, '*『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*', `Hai,Aku Adalah Bot Yang Diberi Nama Alfarabotz,Aku Di Kembangkan Oleh Seseorang Yang Bernama Irfaan-Official,Beliau Masih Pemula Dan Masih Belajar Banyak Hal,Dan tujuannya Hanya Ingin Tau Apa Yang Ingin Di ketahui Di Dunia Bot,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://youtube.com/channel/UCaFh82MyrVgcgIvJxvTA39w', 'ʏᴏᴜᴛᴜʙᴇ ᴏᴡɴᴇʀ', null, null, [
 [`ᴍᴇɴᴜ`, '.m'],
-[`\nɢᴡᴇʜ ꜱᴀᴍɴɢᴇ ᴅᴇɴɢᴀɴ ᴀɴɪᴍᴇʜ`, 'Bilek']
+[`\n.donasi`, 'Bilek']
 ], m, {asLocation: true})
 }
 handler.help = ['menu']
