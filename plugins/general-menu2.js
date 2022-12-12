6d152f5388060cf1abbbd0656854edaf44d7e244
Johannes <92708671+Johannes2803@users.noreply.github.com>
2022-12-12 13:00:56 +0000

    Change Menu

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -1,14 +1,159 @@
-import moment from 'moment-timezone'
-let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
-let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
-let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
-conn.sendHydrated(m.chat, '*『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*', `ʜᴀɪ,ᴀᴋᴜ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ʏᴀɴɢ ᴅɪʙᴇʀɪ ɴᴀᴍᴀ ꜱᴀᴅ-ʙᴏᴛ,ᴀᴋᴜ ᴅɪ ᴋᴇᴍʙᴀɴɢᴋᴀɴ ᴏʟᴇʜ ꜱᴇꜱᴇᴏʀᴀɴɢ ʏᴀɴɢ ʙᴇʀɴᴀᴍᴀ ᴘᴀᴘᴀʜ-ᴄʜᴀɴ,ʙᴇʟɪᴀᴜ ᴍᴀꜱɪʜ ᴘᴇᴍᴜʟᴀ ᴅᴀɴ ᴍᴀꜱɪʜ ʙᴇʟᴀᴊᴀʀ ʙᴀɴʏᴀᴋ ʜᴀʟ,ᴅᴀɴ ᴛᴜᴊᴜᴀɴɴʏᴀ ʜᴀɴʏᴀ ɪɴɢɪɴ ᴛᴀᴜ ᴀᴘᴀ ʏᴀɴɢ ɪɴɢɪɴ ᴅɪᴋᴇᴛᴀʜᴜɪ ᴅɪ ᴅᴜɴɪᴀ ʙᴏᴛ,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'bit.ly/Papah-Chan', 'ʏᴏᴜᴛᴜʙᴇ ᴏᴡɴᴇʀ', null, null, [
-[`ᴍᴇɴᴜ`, '.m'],
-[`\nɢᴡᴇʜ ꜱᴀᴍɴɢᴇ ᴅᴇɴɢᴀɴ ᴀɴɪᴍᴇʜ`, 'Bilek']
-], m, {asLocation: true})
-}
-handler.help = ['menu']
-handler.tags = ['general']
-handler.command = /^(menu)$/i
+import moment from "moment-timezone";
+import fs from "fs";
+import fetch from "node-fetch";
+import jimp from "jimp";
+import PhoneNumber from "awesome-phonenumber";
+let { MessageType } = (await import("@adiwajshing/baileys")).default;
 
-export default handler
+let handler = async (m, { conn, usedPrefix: _p, __dirname, text, command }) => {
+  let tag = `@${m.sender.replace(/@.+/, "")}`;
+  let mentionedJid = [m.sender];
+  let ucpn = `${ucapan()}`;
+  let name = conn.getName(m.sender);
+
+  //tim
+  let wib = moment.tz("Asia/Jakarta").format("HH:mm:ss");
+  let wibh = moment.tz("Asia/Jakarta").format("HH");
+  let wibm = moment.tz("Asia/Jakarta").format("mm");
+  let wibs = moment.tz("Asia/Jakarta").format("ss");
+  let wit = moment.tz("Asia/Jayapura").format("HH:mm:ss");
+  let wita = moment.tz("Asia/Makassar").format("HH:mm:ss");
+  let wktuwib = `${wibh} H ${wibm} M ${wibs} S`;
+
+  let d = new Date(new Date() + 3600000);
+  let locale = "id";
+  // d.getTimeZoneOffset()
+  // Offset -420 is 18.00
+  // Offsetalldiii0 is  0.00
+  // Offset  420 is  7.00
+  let weton = ["Pahing", "Pon", "Wage", "Kliwon", "Legi"][
+    Math.floor(d / 84600000) % 5
+  ];
+  let week = d.toLocaleDateString(locale, { weekday: "long" });
+  let date = d.toLocaleDateString(locale, {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  //==============> Menu nya
+  let intro = `ʜᴀɪ,ᴀᴋᴜ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ʏᴀɴɢ ᴅɪʙᴇʀɪ ɴᴀᴍᴀ ꜱᴀᴅ-ʙᴏᴛ,ᴀᴋᴜ ᴅɪ ᴋᴇᴍʙᴀɴɢᴋᴀɴ ᴏʟᴇʜ ꜱᴇꜱᴇᴏʀᴀɴɢ ʏᴀɴɢ ʙᴇʀɴᴀᴍᴀ ᴘᴀᴘᴀʜ-ᴄʜᴀɴ,ʙᴇʟɪᴀᴜ ᴍᴀꜱɪʜ ᴘᴇᴍᴜʟᴀ ᴅᴀɴ ᴍᴀꜱɪʜ ʙᴇʟᴀᴊᴀʀ ʙᴀɴʏᴀᴋ ʜᴀʟ,ᴅᴀɴ ᴛᴜᴊᴜᴀɴɴʏᴀ ʜᴀɴʏᴀ ɪɴɢɪɴ ᴛᴀᴜ ᴀᴘᴀ ʏᴀɴɢ ɪɴɢɪɴ ᴅɪᴋᴇᴛᴀʜᴜɪ ᴅɪ ᴅᴜɴɪᴀ ʙᴏᴛ,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ`;
+  conn.send2ButtonDoc(
+    m.chat,
+    `\n\n     *『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*\n\n`,
+    intro + `\n\n${tag}\n\n`,
+    "Aʟʟ Mᴇɴᴜ",
+    ".m",
+    "\nAku Pedo Ygy",
+    "bilek",
+    m,
+    {
+      contextInfo: {
+        forwardingScore: fsizedoc,
+        externalAdReply: {
+          body: "Tes",
+          containsAutoReply: true,
+          mediaType: 1,
+          mediaUrl: hwaifu.getRandom(),
+          renderLargerThumbnail: true,
+          showAdAttribution: true,
+          sourceId: "Tes",
+          sourceType: "PDF",
+          previewType: "PDF",
+          sourceUrl: sgc,
+          thumbnail: fs.readFileSync("./thumbnail.jpg"),
+          thumbnailUrl: sgc,
+          title: "*ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ*",
+        },
+      },
+    }
+  );
+};
+
+handler.help = ["menu"];
+handler.tags = ["main"];
+handler.command = /^(menu|help|co)$/i;
+handler.register = false;
+
+export default handler;
+
+//----------- FUNCTION -------
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+const more = String.fromCharCode(8206);
+const readMore = more.repeat(4001);
+
+function clockString(ms) {
+  let h = isNaN(ms) ? "--" : Math.floor(ms / 3600000);
+  let m = isNaN(ms) ? "--" : Math.floor(ms / 60000) % 60;
+  let s = isNaN(ms) ? "--" : Math.floor(ms / 1000) % 60;
+  return [h, " H ", m, " M ", s, " S "]
+    .map((v) => v.toString().padStart(2, 0))
+    .join("");
+}
+function clockStringP(ms) {
+  let ye = isNaN(ms) ? "--" : Math.floor(ms / 31104000000) % 10;
+  let mo = isNaN(ms) ? "--" : Math.floor(ms / 2592000000) % 12;
+  let d = isNaN(ms) ? "--" : Math.floor(ms / 86400000) % 30;
+  let h = isNaN(ms) ? "--" : Math.floor(ms / 3600000) % 24;
+  let m = isNaN(ms) ? "--" : Math.floor(ms / 60000) % 60;
+  let s = isNaN(ms) ? "--" : Math.floor(ms / 1000) % 60;
+  return [
+    ye,
+    " *Years 🗓️*\n",
+    mo,
+    " *Month 🌙*\n",
+    d,
+    " *Days ☀️*\n",
+    h,
+    " *Hours 🕐*\n",
+    m,
+    " *Minute ⏰*\n",
+    s,
+    " *Second ⏱️*",
+  ]
+    .map((v) => v.toString().padStart(2, 0))
+    .join("");
+}
+function ucapan() {
+  const time = moment.tz("Asia/Jakarta").format("HH");
+  let res = "Sudah Dini Hari Kok Belum Tidur Kak? 🥱";
+  if (time >= 4) {
+    res = "Pagi Lord 🌄";
+  }
+  if (time >= 10) {
+    res = "Selamat Siang Kak ☀️";
+  }
+  if (time >= 15) {
+    res = "Selamat Sore Kak 🌇";
+  }
+  if (time >= 18) {
+    res = "Malam Kak 🌙";
+  }
+  return res;
+}
+
+function wish() {
+  let wishloc = "";
+  const time = moment.tz("Asia/Kolkata").format("HH");
+  wishloc = "Hi";
+  if (time >= 0) {
+    wishloc = "Night Rider";
+  }
+  if (time >= 4) {
+    wishloc = "Good Morning";
+  }
+  if (time >= 12) {
+    wishloc = "Good Afternoon";
+  }
+  if (time >= 16) {
+    wishloc = "️Good Evening";
+  }
+  if (time >= 23) {
+    wishloc = "Night Rider";
+  }
+  return wishloc;
+}
